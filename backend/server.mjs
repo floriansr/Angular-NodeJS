2091ae48dca6e31cf95f8ab830b0abba3324db85
floriansr <sueurfloriansf@gmail.com>
2020-08-10 13:24:10 +0200

    [change] ES6 module

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,5 +1,6 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
+import { createServer } from 'http'
+
+const server = createServer((req, res) => {
    res.end("voila la réponse du premier serveur")
 })
 
