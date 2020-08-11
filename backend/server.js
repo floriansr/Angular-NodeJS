00d9366539d72aa89540bddc4504eba2e232a9a9
floriansr <sueurfloriansf@gmail.com>
2020-08-11 11:02:52 +0200

    [fix] .mjs extension to .js extension

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import { createServer } from 'http'
+import app from './app.js'
+
+app.set("port", process.env.PORT || 3000);
+
+const server = createServer(app);
+
+server.listen(process.env.PORT || 3000);
--- backend/server.mjs
+++
@@ -1,7 +0,0 @@
-import { createServer } from 'http'
-
-const server = createServer((req, res) => {
-   res.end("voila la réponse du premier serveur")
-})
-
-server.listen(process.env.PORT || 3000)
