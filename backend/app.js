68ba1004528d372fb5956dc98307b801bbf4e094
floriansr <sueurfloriansf@gmail.com>
2020-08-11 11:18:53 +0200

    [add] express middlewares

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express()
 
-app.use((req, res) => {
-  res.json({message: "voici le json"})
+app.use((req, res, next) => {
+  console.log("Response sent !")
+  next()
 })
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "response" })
+  next()
+})
+
+app.use((req, res) => {
+  console.log("Success!");
+});
 
 export default app
