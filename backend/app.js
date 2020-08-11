8bc06edde0e939a6872ad57e4739b672bf565510
floriansr <46346593+floriansr@users.noreply.github.com>
2020-08-11 12:11:39 +0200

    Create post (#2)
    * [add] body-parser packages
    * [add] POST request

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express()
 
@@ -7,6 +8,15 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+app.use(bodyParser.json());
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
 });
 
 app.use('/api/stuff', (req, res) => {
