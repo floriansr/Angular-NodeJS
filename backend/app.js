e4c07eafe46f3509f52f292f58293a3929ca573f
floriansr <sueurfloriansf@gmail.com>
2020-09-02 00:04:45 +0200

    [refacto] api_manager_stuff.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import Thing from "./models/Thing.js"
 import mongoose from "mongoose";
+import stuffRoutes from './router/stuff.js';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -28,40 +28,7 @@ app.use((req, res, next) => {
 //PARSER
 app.use(bodyParser.json());
 
-//CREATE
-app.post("/api/stuff", (req, res) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  })
-  thing.save().then(() => res.status(201).json({message: "Objet enregistré"})).catch(error => res.status(400).json({error}))
-});
+app.use('/api/stuff', stuffRoutes);
 
-//READ
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-//MODIFICATE
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//DELETE
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-    .catch(error => res.status(400).json({ error }));
-});
 
 export default app
