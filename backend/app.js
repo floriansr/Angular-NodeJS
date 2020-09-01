1ebc3e1c108df7de4731e4ddc54e5d6f776e25c1
floriansr <46346593+floriansr@users.noreply.github.com>
2020-09-01 15:35:29 +0200

    [add] updateOne express (#5)

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 const app = express()
 
+//MONGODB
 mongoose
   .connect(
     `mongodb+srv://floriansr:${process.env.MONGODB_PASSWORD}@cluster0.ozjxx.mongodb.net/<dbname>?retryWrites=true&w=majority`,
@@ -16,6 +17,7 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+//CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -23,8 +25,10 @@ app.use((req, res, next) => {
   next();
 });
 
+//PARSER
 app.use(bodyParser.json());
 
+//CREATE
 app.post("/api/stuff", (req, res) => {
   delete req.body._id;
   const thing = new Thing({
@@ -33,10 +37,18 @@ app.post("/api/stuff", (req, res) => {
   thing.save().then(() => res.status(201).json({message: "Objet enregistré"})).catch(error => res.status(400).json({error}))
 });
 
+//READ
 app.get("/api/stuff/:id", (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then((thing) => res.status(200).json(thing))
     .catch((error) => res.status(404).json({ error }));
+});
+
+//MODIFICATE
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.use('/api/stuff', (req, res) => {
