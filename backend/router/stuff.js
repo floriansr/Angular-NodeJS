9594e317b967b424842a628c81b1e014ee371137
floriansr <sueurfloriansf@gmail.com>
2020-09-02 19:18:03 +0200

    [erase] CRUD from stuffRouter

--- backend/router/stuff.js
+++ backend/router/stuff.js
@@ -1,42 +1,14 @@
 import express from "express"
-import Thing from '../models/thing.js'
+import stuffCtrl from "../controllers/stuff.js";
+import auth from "../middlewares/auth.js"
 
-const router = express.Router();
+const router = express.Router()
 
-//CREATE
-router.post("/", (req, res) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  })
-  thing.save().then(() => res.status(201).json({ message: "Objet enregistré" })).catch(error => res.status(400).json({ error }))
-});
 
-//READ
-router.get("/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-router.get('/', (req, res) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-//MODIFICATE
-router.put('/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//DELETE
-router.delete('/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-    .catch(error => res.status(400).json({ error }));
-});
+router.post("/", auth, stuffCtrl.createThing);
+router.get("/:id", auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllThings);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 export default router
