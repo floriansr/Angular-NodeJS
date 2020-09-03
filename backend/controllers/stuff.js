963212368c0f996d7f1d04500dcb535ecd64ebc0
floriansr <sueurfloriansf@gmail.com>
2020-09-03 20:26:02 +0200

    [add] route PUT updateThing

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -31,7 +31,18 @@ router.getAllThings = ("/", (req, res) => {
 
 //MODIFICATE
 router.modifyThing = ("/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  const thingObject = req.file
+    ? {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  Thing.updateOne(
+    { _id: req.params.id },
+    { ...thingObject, _id: req.params.id }
+  )
     .then(() => res.status(200).json({ message: "Objet modifié !" }))
     .catch((error) => res.status(400).json({ error }));
 });
--- backend/router/stuff.js
+++ backend/router/stuff.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.get("/", auth, stuffCtrl.getAllThings);
 router.post("/", auth, multer, stuffCtrl.createThing);
 router.get("/:id", auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 export default router
