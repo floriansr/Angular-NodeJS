24b7c4a530153c8f73504e2f2523919e2e685e94
floriansr <sueurfloriansf@gmail.com>
2020-09-03 20:16:32 +0200

    [fix] route POST createThing

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -4,12 +4,16 @@ import Thing from "../models/thing.js";
 const router = express.Router()
 
 //CREATE
-router.createThing = ("/", (req, res) => {
-  delete req.body._id;
+router.createThing = ("/", (req, res, next) => {
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
   const thing = new Thing({
-    ...req.body
-  })
-  thing.save().then(() => res.status(201).json({ message: "Objet enregistré" })).catch(error => res.status(400).json({ error }))
+    ...thingObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 //READ
